27bdf5a799365a033999b67539b2ee681e3acb39
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-06-15 14:33:27 +0200

    add file upload post

--- post.cpp
+++ post.cpp
@@ -1,4 +1,5 @@
 #include "request.hpp"
+#include "response.hpp"
 #include <fcntl.h>
 
 std::string get_post(t_request request) {
@@ -7,13 +8,33 @@ std::string get_post(t_request request) {
 
 	unsigned int start = 0;
 	while (start < request.body.size() - 1) {
-		if (request.body[start] == '\n' && request.body[start + 1] == '\n') {
+		if (request.body[start] == '\n' && request.body[start + 1] == '\r') {
 			break;
 		}
 		start++;
 	}
+	start += 3;
+	unsigned int end = request.body.size() - 1;
+	while (end > 0) {
+		std::cout << "end: " << end << "\n";
+		if (request.body[end] == '\r' && request.body[end - 1] == '\n') {
+			break;
+		}
+		end--;
+	}
+	std::cout << "size: " << request.body.size() << "\n";
+	std::cout << "start: " << start << "\n";
+	std::cout << "file: " << request.body.substr(start, end - start)  << "\n";
 	std::cout << "saving file!\n";
-	int fd = open("post_output_test", O_WRONLY);
-	write (fd, &request.body[start], request.body.size() - start);
-	return ("your file has been saved!\n");
+	int fd = open("tests/post_output_test", O_WRONLY | O_CREAT | O_TRUNC, 0700);
+	if (fd < 0)
+		perror("");
+	std::cout << "fd" << fd << "\n";
+	write (fd, &request.body[start], end - start);
+	close(fd);
+	std::cout << "done!\n";
+	t_response response;
+	response.body = "your file has been saved!\n";
+	response.code = 200;
+	return response_to_string(response);
 }
--- response.hpp
+++ response.hpp
@@ -5,6 +5,7 @@ struct t_response {
 	int code;
 };
 
+std::string response_to_string(t_response resp);
 std::string get_reponse();
 std::string get_reponse_from_page(std::string webpage);
 std::string not_found();
